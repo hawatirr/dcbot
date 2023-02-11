4d9b1b85ea53420efaff6777569d5b59603dd629
diasiapadude <86511205+hawatirr@users.noreply.github.com>
2023-02-12 03:42:07 +0700

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,55 +1,28 @@
 import discord
-import json
+import yaml
 
-class DiscordBot:
-    def __init__(self, token):
-        self.token = token
-        self.client = discord.Client()
+with open("config.yaml", "r") as stream:
+    config = yaml.safe_load(stream)
 
-    async def run(self):
-        @self.client.event
-        async def on_ready():
-            print(f'Logged in as {self.client.user}')
+client = discord.Client()
 
-        @self.client.event
-        async def on_member_join(member):
-            print(f'{member} has joined the server.')
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
 
-        @self.client.event
-        async def on_member_remove(member):
-            print(f'{member} has left the server.')
+if config['message_on_join']:
+    @client.event
+    async def on_member_join(member):
+        for channel_id in config['channel_ids']:
+            channel = client.get_channel(channel_id)
+            await channel.send(f'{member} has joined the server.')
 
-        await self.client.start(self.token)
+if config['message_on_leave']:
+    @client.event
+    async def on_member_remove(member):
+        for channel_id in config['channel_ids']:
+            channel = client.get_channel(channel_id)
+            await channel.send(f'{member} has left the server.')
 
-def load_config(filename):
-    try:
-        with open(filename, 'r') as f:
-            config = json.load(f)
-        return config
-    except FileNotFoundError:
-        return {}
-
-def save_config(filename, config):
-    with open(filename, 'w') as f:
-        json.dump(config, f, indent=4)
-
-def main():
-    config = load_config('config.json')
-    tokens = config.get('tokens', [])
-
-    while True:
-        token = input('Enter a Discord bot token (or "done" to finish): ')
-        if token == 'done':
-            break
-        tokens.append(token)
-
-    config['tokens'] = tokens
-    save_config('config.json', config)
-
-    bots = [DiscordBot(token) for token in tokens]
-
-    for bot in bots:
-        bot.run()
-
-if __name__ == '__main__':
-    main()
+for bot_token in config['bot_tokens']:
+    client.run(bot_token)
